0a3ddb90cf7fe2fa90bc3ef30b1c432cfc765cdd
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-15 15:35:48 -0500

    Buscar dentro de una Cadena de Caracteres
    Videoreceta Python No. 1-3: Buscar dentro de una Cadena de Caracteres

---
+++ Capitulo1/r103_buscar_cadena.py
@@ -0,0 +1,8 @@
+# 1. A través del operador polimórfico in
+# 2. A través del método find (string)
+
+lenguaje = 'Python es un lenguaje de programación'
+
+print('python' in lenguaje)
+
+print(lenguaje.find('programación'))
