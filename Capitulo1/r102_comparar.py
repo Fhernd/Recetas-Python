9d1092560a148dd6c6da8681577ff38353f217fb
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-15 15:35:13 -0500

    Comparar Cadenas de Caracteres
    Videoreceta Python No. 1-2: Comparar Cadenas de Caracteres

---
+++ Capitulo1/r102_comparar.py
@@ -0,0 +1,18 @@
+# ==: a nivel de contenido
+# is: a nivel de referencia
+
+lenguaje = 'Python'
+
+if lenguaje == 'python':
+    print('Las cadenas de caracteres son iguales')
+else:
+    print('Las cadenas de caracteres NO son iguales')
+
+
+lenguaje_python = lenguaje
+
+if lenguaje_python is lenguaje:
+    print('Las referencias son iguales')
+else:
+    print('Las referencias no son iguales.')
+
