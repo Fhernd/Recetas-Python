0103618998d2cb7fdb456c9bf8d74c6a581e217e
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-18 17:20:04 -0500

    Receta 1-10: Iterar una Cadena de Caracteres
    Iterar una Cadena de Caracteres utilizando el lenguaje de programación Python.

---
+++ Capitulo1/r110_iterar_cadena.py
@@ -0,0 +1,14 @@
+
+# Iterar una cadena de caracteres:
+
+# 1. Utilizar los índices a través de un ciclo for
+# 2. Utilizar un iterador
+
+frase = 'Python 3.7'
+
+for i in range(len(frase)):
+    print(frase[i])
+
+
+for caracter in frase:
+    print(caracter)
