d5c0cec2b4eefc4e87af7142e31779c35606957c
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-19 07:13:02 -0500

    Receta 1-11: Calcular Estadísticas Básicas sobre una Cadena de Caracteres
    Convertir Cadenas de Caracteres en Valores Numéricos en lenguaje de programación Python.

---
+++ Capitulo1/r111_estadisticas_cadena_caracteres.py
@@ -0,0 +1,15 @@
+
+# Calcular estadísticas básicas sobre una cadena de caracteres:
+
+
+# longitud, el carácter máximo y mínimo, y el número de ocurrencias de un carácter:
+
+frase = 'Python es un lenguaje de programación'
+
+print(len(frase))
+
+print(min(frase))
+
+print(max(frase))
+
+print(frase.count('o'))
