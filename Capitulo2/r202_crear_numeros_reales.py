4ebfde352092a13870be4efda151af1bc55b3efc
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-20 14:35:33 -0500

    R2-2: Crear números reales
    R2-2: Crear números reales

---
+++ Capitulo2/r202_crear_numeros_reales.py
@@ -0,0 +1,18 @@
+
+# Receta 2-2: Crear números reales
+
+# 1. A través de literales
+# 2. Es por medio de la función float()
+
+decimal1 = 3.1415
+
+decimal2 = float('3.1415')
+
+decimal3 = float(input('Escriba un número real: '))
+
+decimal4 = 1e3 # 1000
+
+print(decimal1)
+print(decimal2)
+print(decimal3)
+print(decimal4)
