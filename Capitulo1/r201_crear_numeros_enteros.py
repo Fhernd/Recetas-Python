25cf64385abd70e95345413d9db64de9d004ab0b
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-20 11:05:37 -0500

    R2-1: Crear Números Enteros
    R2-1: Crear Números Enteros

---
+++ Capitulo1/r201_crear_numeros_enteros.py
@@ -0,0 +1,20 @@
+
+# Receta 2-1: Crear números enteros
+
+# 1. literales
+# 2. entrada del usuario
+
+entero = 29
+
+numero = int(input('Escriba un número: '))
+
+print(entero, numero)
+
+print(numero.bit_length())
+
+binario = '111'
+
+representacion_entera = int(binario, 2)
+
+print(representacion_entera)
+
