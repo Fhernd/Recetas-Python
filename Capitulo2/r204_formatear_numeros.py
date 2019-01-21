5882123118f6eb388ddc36125e3920a2070aa845
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-21 09:48:04 -0500

    R2-4: Formatear números
    R2-4: Formatear números

---
+++ Capitulo2/r204_formatear_numeros.py
@@ -0,0 +1,16 @@
+
+# Receta 2-4: Formatear números
+
+# format()
+
+entero = 42
+decimal = 31415.9265
+porcentaje = 0.19
+
+print(format(entero, 'X'))
+print(format(decimal, 'e'))
+print(format(decimal, '.2f'))
+print(format(porcentaje, '%'))
+
+# https://docs.python.org/3.7/library/functions.html?highlight=format#format
+# https://docs.python.org/3.7/library/string.html#formatspec
