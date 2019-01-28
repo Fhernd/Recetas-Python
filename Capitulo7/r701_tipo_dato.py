101e8f380d6b261a552d09606a3581ebe8963393
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-01-28 16:26:03 -0500

    R701: Obtener el tipo de data de una variable
    R701: Obtener el tipo de data de una variable

---
+++ Capitulo7/r701_tipo_dato.py
@@ -0,0 +1,14 @@
+
+# Receta Python 7-1: Obtener el tipo de dato de una variable o de una literal.
+
+# 1. type()
+# 2. isinstance()
+
+a = 47
+b = 'Python'
+
+print(type(a))
+print(type(b))
+
+print(isinstance(a, str))
+print(isinstance(b, int))
